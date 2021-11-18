62bf8887b97fcde3b9ecac78af6b7ed1908a42ab
Tanner Van De Walle <tvandewalle@microsoft.com>
2021-11-18 03:51:54 -0800

    A basic passing test to verify that it works

---
+++ tst/HelloTest.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+namespace D3D9on12::Test
+{
+    TEST(HelloTest, BasicPassingTest)
+    {
+        EXPECT_EQ(0, 0);
+    }
+}
